cf7e5aa52ae75735b62881e9fd14fdd87b00ea69
Ant <huertinez.family@outlook.com>
2021-12-15 16:38:22 -0800

    update tests

--- src/components/About/__tests__/index.test.js
+++ src/components/About/__tests__/index.test.js
@@ -1,10 +1,9 @@
 import React from 'react';
-
 import { render, cleanup } from '@testing-library/react';
-
 import '@testing-library/jest-dom/extend-expect';
-
 import About from '..';
+
+afterEach(cleanup);
 
 describe('About component', () => {
     //First Test
@@ -19,5 +18,3 @@ describe('About component', () => {
     })
 
 })
-
-afterEach(cleanup);
--- src/components/Nav/index.js
+++ src/components/Nav/index.js
@@ -2,17 +2,10 @@ import React from "react";
 
 function Nav() {
   const categories = [
-    {
-      name: "commercial",
-      description:
-        "Photos of grocery stores, food trucks, and other commercial projects",
-    },
-    { name: "portraits", description: "Portraits of people in my life" },
-    { name: "food", description: "Delicious delicacies" },
-    {
-      name: "landscape",
-      description: "Fields, farmhouses, waterfalls, and the beauty of nature",
-    },
+  { name: 'commercial', description: 'Photos of grocery stores, food trucks, and other commercial projects' },
+    { name: 'portraits', description: 'Portraits of people in my life' },
+    { name: 'food', description: 'Delicious delicacies' },
+    { name: 'landscape', description: 'Fields, farmhouses, waterfalls, and the beauty of nature' }
   ];
 
   function categorySelected(name) {
@@ -22,14 +15,14 @@ function Nav() {
   return (
     <header>
   <h2>
-    <a href="/">
+    <a data-testid="link" href="/">
       <span role="img" aria-label="camera"> 📸</span> Oh Snap!
     </a>
   </h2>
       <nav>
         <ul className="flex-row">
           <li className="mx-2">
-            <a href="#about">About me</a>
+            <a data-testid="about" href="#about" onClick>About me</a>
           </li>
           <li>
             <span>Contact</span>
